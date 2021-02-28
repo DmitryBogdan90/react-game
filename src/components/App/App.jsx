94a05b823d326d3fe0afb6bebe238be70c853a9d
DmitryBogdan <mgblsttt@gmail.com>
2021-02-28 15:49:56 +0300

    refactor: change App component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { CssBaseline } from '@material-ui/core';
+import './App.scss';
 
 const App = () => {
   return (
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
 import './style.scss';
+
+import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
