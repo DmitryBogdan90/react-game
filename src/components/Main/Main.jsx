6698fd68bbe3d4a3ae58b7d27ad092aa64bb5cf7
DmitryBogdan <mgblsttt@gmail.com>
2021-02-28 18:28:23 +0300

    feat: add game mechanics with hooks

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './Main.scss';
+import { Button } from '@material-ui/core';
 
 const Main = () => {
-  return <main>MAIN</main>;
+  const defaultChoices = ['Rock', 'Scissors', 'Paper'];
+  const extendedChoices = ['Rock', 'Scissors', 'Paper', 'Lizard', 'Spock'];
+
+  const [playerChoice, setPlayerChoice] = useState('default');
+  const [browserChoice, setBrowserChoice] = useState('browserChoice');
+
+  useEffect(() => {
+    setBrowserChoice(defaultChoices[Math.floor(Math.random() * 3)]);
+  }, [playerChoice]);
+
+  const handleClick = (choice) => {
+    console.log('choice');
+    setPlayerChoice('');
+    setPlayerChoice(choice);
+  };
+
+  return (
+    <main>
+      <div>Browser choice: {browserChoice}</div>
+      <div>Player choice: {playerChoice}</div>
+      {defaultChoices.map((choice, index) => {
+        return (
+          <Button key={index} onClick={() => handleClick(choice)}>
+            {choice}
+          </Button>
+        );
+      })}
+    </main>
+  );
 };
 
 export default Main;
