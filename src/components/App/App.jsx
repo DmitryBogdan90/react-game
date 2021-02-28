5628756c0c86a4dd0062b4ad908991c2415668b3
DmitryBogdan <mgblsttt@gmail.com>
2021-02-28 15:51:10 +0300

    feat: add Header

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { CssBaseline } from '@material-ui/core';
+
 import './App.scss';
+import Header from '../Header';
 
 const App = () => {
   return (
     <CssBaseline>
+      <Header />
       <div>React-game</div>
     </CssBaseline>
   );
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Header.scss';
+
+const Header = () => {
+  return <header>Header</header>;
+};
+
+export default Header;
